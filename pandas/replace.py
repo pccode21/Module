0f33af5f5631b937b1e040339ebc302032f52994
LinXuDong <57325127+pccode21@users.noreply.github.com>
2020-04-30 17:31:42 +0800

    Update replace.py

--- pandas/replace.py
+++ pandas/replace.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 d = {"customer": ["A", "B", "C", "D"], "sales":[1100, "950.5RMB", "$400", " $1250.75"]}
 df = pd.DataFrame(d)
@@ -6,6 +7,11 @@ print(df)
 df["sales"] = df["sales"].replace('[$, RMB]', '', regex=True).astype('float')
 # 使用正则替换，将要替换的字符放到列表中 [$,RMB]，替换为空字符，即 "",最后使用 astype 转为 float
 print(df)
+s = pd.DataFrame(list('ascaazsd'))
+print(s)
+print(s.replace('a', 'new_a'))  # 替换单个
+print(s.replace(['a', 's'], np.nan))  # 传入列表，替换多个
+print(s.replace({'a': 'new_a', 's': 'new_s'}))  # 传入字典，分别替换
 """
   customer      sales
 0        A       1100
